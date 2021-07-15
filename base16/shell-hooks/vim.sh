1574af792cb3ed9c1c27994823b8b5772127db0c
Martijn van Maurik <martijn.van.maurik@on2it.net>
2021-07-15 11:03:42 +0200

    Update kitty sound, awesome keybinds and some vim airline support

--- base16/shell-hooks/vim.sh
+++ base16/shell-hooks/vim.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cat > ~/.vimrc_background <<EOF
+cat > ~/.vimrc_theme <<EOF
 if !exists('g:colors_name') || g:colors_name != 'base16-${BASE16_THEME}'
   colorscheme base16-${BASE16_THEME}
 endif
 EOF
 
-echo -e "\e[93mUpdated \e[31m.vimrc_background\e[0m"
+echo -e "\e[93mUpdated \e[31m.vimrc_theme\e[0m"
