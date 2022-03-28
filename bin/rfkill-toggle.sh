8ee37107d68d3be97f07eac5f85cae51161b347c
Martijn van Maurik <martijn.van.maurik@on2it.net>
2022-03-28 15:19:21 +0200

    Update dotfiles

--- bin/rfkill-toggle.sh
+++ bin/rfkill-toggle.sh
@@ -4,11 +4,18 @@
 # where IDENTIFIER is the index no. of an rfkill switch or one of:
 #   wifi wlan bluetooth uwb ultrawideband wimax wwan gps fm nfc
 
-ID=`rfkill list "$1" | head -c 1 | cut -f 1`
+if [ -z $1 ]; then
+    echo -n "enter a valid identifier; possible identifier: "
+    rfkill list --output type -n | paste -s -d, -
+    exit 1
+fi
+
+ID=`rfkill list "$1" 2>/dev/null | head -c 1 | cut -f 1`
 SOFT="/sys/class/rfkill/rfkill$ID/soft"
 
 if [ ! -f "$SOFT" ]; then
-    echo "no such identifier"
+    echo -n "no such identifier; possible identifier: "
+    rfkill list --output type -n | paste -s -d, -
     exit 1
 fi
 
