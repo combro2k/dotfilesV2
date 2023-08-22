f0e1afb8f8486719bd71cb2efbad1f1005409081
combro2k <github.t7k0x@obfuscation.work>
2023-08-22 10:39:02 +0200

    Update

--- fish/conf.d/60-abbr.fish
+++
@@ -1,5 +0,0 @@
-abbr -a -- gco            'git checkout'
-abbr -a -- gcu            '~/.local/bin/git-cleanup.sh'
-abbr -a -- gbom           '~/.local/bin/git-base-on-master.sh'
-abbr -a -- vi             'vim'
-abbr -a -- docker-compose 'docker compose'
